1549bc3685b102bb3c9864eb82051382c836be29
csehg0312 <62523554+csehg0312@users.noreply.github.com>
2023-03-11 19:02:14 +0100

    Defining types

---
+++ defining_types.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.Scanner;
+
+class HelloWorld
+{
+    public static void main(String args[]) {
+    int num = 10;
+    String meet = "Hello World!";
+    byte en = 01;
+    short only = 2;
+    long number = 121315;
+    float like = 5.76f;
+    double duo = 6.95d;
+    char firstone = 'A';
+    char between = '\n';
+    boolean bowl = false;
+    Object Person = null;
+    
+    System.out.println(meet+between+num+between+en+between+only+between+number+between+like+between+duo+between+firstone+between+bowl);
+
+
+}
+}
+
